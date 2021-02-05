83f4f83e85afff7552da2023256190b969b78c8f
Beth Spencer <beth.spencer@student.labette.edu>
2021-02-04 22:11:15 -0600

    add service-worker

---
+++ Unsolved/public/service-worker.js
@@ -0,0 +1,70 @@
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/db.js",
+  "/style.css",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were successfully cached!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+//activate
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Clearing old cache!", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+//fetch
+self.addEventListener("fetch", (e) => {
+  if (e.request.url.includes("/api/")) {
+    console.log("[Service Worker] Fetch(data)", e.request.url);
+
+    e.respondWith(
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(e.request)
+          .then((response) => {
+            if (response.status === 200) {
+              cache.put(e.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch((err) => {
+            return cache.match(e.request);
+          });
+      })
+    );
+    return;
+  }
+
+  e.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(e.request).then((response) => {
+        return response || fetch(e.request);
+      });
+    })
+  );
+});
